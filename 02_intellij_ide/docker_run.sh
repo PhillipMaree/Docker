9b8bdcedc24981553171dac50dca86c85b99128a
Phillip Maree <Phillip.Maree@sintef.no>
2020-03-25 16:37:50 +0100

    Bind mounted volume.

--- 02_intellij_ide/docker_run.sh
+++ 02_intellij_ide/docker_run.sh
@@ -1,25 +1,4 @@
 #!/bin/bash
+echo -e '\n \e[1;34mMounted project base directory:\033[0m\e[1;33m'$PWD'\033[0m\n'
+docker run -it --privileged --volume=$HOME"/.Xauthority:/root/.Xauthority:rw" --mount type=bind,source=$PWD,target=/home/$USER --env="DISPLAY" --net=host jpmaree/optimization_ide:latest
 
-# default work directory
-export MOUNT_DIR=$PWD"/mnt"
-
-# process options
-
-while test $# -gt 0; do
-  case "$1" in
-    -w)
-      shift
-      if test $# -gt 0; then
-        export MOUNT_DIR=$1
-        shift
-      else
-        echo -e "\e[1;31mNo directory path to process. Usage ./docker_run.sh -w path\033[0m"
-        exit 1
-      fi
-      ;;
-    esac
-done
-
-# run docker container sharing host's XServer
-
-docker run -it --privileged --volume=$HOME"/.Xauthority:/root/.Xauthority:rw" --volume=$MOUNT_DIR":/home/"$USER --env="DISPLAY" --net=host jpmaree/optimization_ide:latest
